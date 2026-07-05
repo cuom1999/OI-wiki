1978e69dc47d32972b1ed301b66ae0d3eb314b19
cuom1999 <lephuocdinh99@gmail.com>
2026-07-04 21:01:41 -0700

    Localize site chrome to Vietnamese

--- hooks/on_env.py
+++ hooks/on_env.py
@@ -1,5 +1,27 @@
 import re
 
+HTML_TRANSLATIONS = {
+    "Switch to dark mode": "Chuyển sang chế độ tối",
+    "Switch to light mode": "Chuyển sang chế độ sáng",
+    'aria-label="Tabs"': 'aria-label="Thẻ"',
+    'title="Permanent link"': 'title="Liên kết cố định"',
+    'title="edit.link.title"': 'title="Chỉnh sửa"',
+    "本页面最近更新：": "Trang này được cập nhật gần nhất: ",
+    "更新历史": "lịch sử cập nhật",
+    "</span>，<a": "</span>, <a",
+    "发现错误？想一起完善？ ": "Phát hiện lỗi? Muốn cùng cải thiện? ",
+    "在 GitHub 上编辑此页！": "Chỉnh sửa trang này trên GitHub!",
+    "本页面贡献者：": "Người đóng góp trang này: ",
+    "本页面的全部内容在 ": "Toàn bộ nội dung của trang này được cung cấp theo ",
+    " 协议之条款下提供，附加条款亦可能应用": "; các điều khoản bổ sung cũng có thể áp dụng",
+    "回到顶部": "Về đầu trang",
+    "最近更新：": "Cập nhật gần nhất: ",
+    "Cập nhật gần nhất: , ": "Cập nhật gần nhất: ",
+    "黑ICP备19005132号-2": "Giấy phép ICP Hắc Long Giang 19005132-2",
+    "少年，恭喜你囍提彩蛋，我们在做一些 OI 相关的有趣的事情，如果您对此感兴趣，欢迎访问 https://join-us.oi-wiki.org": "Chúc mừng bạn đã tìm thấy easter egg. Chúng tôi đang làm một số việc thú vị liên quan đến OI; nếu quan tâm, hãy truy cập https://join-us.oi-wiki.org",
+    '"data-lang":"en-US"': '"data-lang":"vi"',
+}
+
 def _nav_math():
     raw_re = r"\\\((.+?)\\\)"
     target = r'<span class="arithmatex">\(\1\)</span>'
@@ -11,3 +33,8 @@ def _nav_math():
 def on_env(env, config, files, **kwargs):
     env.filters["nav_math"] = _nav_math()
     return env
+
+def on_post_page(output, page, config, **kwargs):
+    for source, target in HTML_TRANSLATIONS.items():
+        output = output.replace(source, target)
+    return output
